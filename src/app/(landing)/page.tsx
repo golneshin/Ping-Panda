f2270145c50c2ec015b343dcd9291f393232bbd0
golneshin <59548304+golneshin@users.noreply.github.com>
2025-01-26 10:38:14 +0330

    auth pages: signin and signup, have been created.

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -29,7 +29,7 @@ const page = () => {
 
   return (
     <>
-      {/* landing head texts */}
+      {/* hero section */}
       <section className=" relative py-24 sm:py32 bg-brand-25">
         <MaxWidthWrapper className="text-center">
           <div className="relative mx-auto text-center flex flex-col items-center gap-10">
